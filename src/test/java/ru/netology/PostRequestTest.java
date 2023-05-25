eca1921651e9cd589df8ae8ae55daaa8b09f424a
Slava Vasiliev <sb-fantom@yandex.ru>
2023-05-25 22:21:29 +0300

    change 3 README.md

--- src/test/java/ru/netology/PostRequestTest.java
+++ src/test/java/ru/netology/PostRequestTest.java
@@ -20,7 +20,7 @@ public class PostRequestTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some date"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
