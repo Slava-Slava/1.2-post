28dcff7f7124f82acace73569de231f7075d3c5d
Slava Vasiliev <sb-fantom@yandex.ru>
2023-05-25 22:12:58 +0300

    change 1 README.md

--- src/test/java/ru/netology/PostRequestTest.java
+++ src/test/java/ru/netology/PostRequestTest.java
@@ -20,7 +20,7 @@ public class PostRequestTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some date"))
         ;
     }
 }
